5c337729f291e06212c2a4f9197a3a9c4074ff76
lvbcpa6 <laurent.vanbeveren@colruytgroup.com>
2019-02-20 12:12:42 +0100

    correctionUpdate

--- Logotech.API/Dtos/PatientForUpdateDto.cs
+++ Logotech.API/Dtos/PatientForUpdateDto.cs
@@ -9,10 +9,10 @@ namespace Logotech.API.Dtos
         public string Prenom { get; set; }
         public DateTime DateNaissance { get; set; }
         public string Email { get; set; }
-        public int? TelFixe { get; set; }
-        public int? Gsm { get; set; }
+        public int TelFixe { get; set; }
+        public int Gsm { get; set; }
         public string PersonneContact { get; set; }
-        public int? TelContact { get; set; }
+        public int TelContact { get; set; }
         public string Anamnese { get; set; }
         public string Commentaire { get; set; }
         public string PhotoUrl { get; set; }
